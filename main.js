036ce99f5a4144a152beeaf338e634923c84cbb7
xuanzebin <xuanzebin@126.com>
2018-06-26 20:59:30 +0800

    第一次轮播提交

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 var allButtons=$('#buttons>span')
-var x=$('body')
-console.log(allButtons)
-console.log(x)
+for(let i=0;i<allButtons.length;i++) {
+    $(allButtons[i]).on('click',function(clickSlide) {
+        let index=$(clickSlide.currentTarget).index()
+        let n=index*-600
+        $(images).css({
+            transform:'translateX('+n+'px)'
+        })
+        $(allButtons[i]).addClass('red')
+        .siblings('.red').removeClass('red')
+        num=index
+    })
+}
+var num=0
+let size=allButtons.length
+let timeSlide=setInterval(()=>{
+    num++
+    allButtons.eq(num%size).trigger('click')
+},1000)
+$('.pictureWindows').on('mouseenter',function(){
+    clearInterval(timeSlide)
+})
+$('.pictureWindows').on('mouseleave',function(){
+    timeSlide=setInterval(()=>{
+        num++
+        allButtons.eq(num%size).trigger('click')
+    },1000)
+})
